9c4a5e24924197f68dba422744e62bbf2e03f80f
lucas <lucas.ssantos189@senacsp.edu.br>
2024-06-17 22:18:59 -0300

    execicio

---
+++ 03/exercicio.php
@@ -0,0 +1,40 @@
+<?php
+
+//1 - Crie um array contendo os dias da semana. Em seguida, exiba o terceiro dia da semana.
+//2 - Crie um array contendo meses do ano. Altere o valor do último elemento para Onzembro"
+//3 - Crie um array vazio. Adicione os números de 1 a 5 no array. Depois, adicione o número 6 ao final do array"
+//4 - Utilize um loop foreach para percorrer o array de meses do ano e exibir cada um.
+//5 - Conte quantos elementos o array de dias de semana possui.
+//6 - Utilize a função array_serach() para econtrar a posicao do dia "Sábado" no array e exiba o resultado.
+
+//01
+$dias_da_semana = array("segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domigo");
+print_r($dias_da_semana);
+echo "<br>";
+echo "O terceiro dia da sema é:" . $dias_da_semana[2] . "<br>";
+//02
+$meses_do_ano = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", 'setembro', "outubro", "novembro", "dezembro"];
+$meses_do_ano[count($meses_do_ano) - 1] = "Onzembro";
+echo "O último mês do ano alterado para: " . end($meses_do_ano) . "<br>";
+
+//03
+$numeros = [];
+for($i = 1; $i <= 5; $i++) {
+    $numeros[] = $i;
+}
+$numeros[] = 6;
+print_r($numeros);
+
+//04
+foreach($meses_do_ano as $mes) {
+    echo $mes . " ";
+}
+
+//05
+echo "O array de dias da semana possui: " . count($dias_da_semana) . " dias <br>";
+
+//06
+$posicao_do_sabado = array_search("Sábado", $dias_da_semana);
+echo "A posição do dia de sábado no array é: $posicao_do_sabado";
+
+?>
