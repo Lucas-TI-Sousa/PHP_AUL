82e976954a0ee42dd2c3c2b6c6fe3d373c698ee6
lucas <lucas.ssantos189@senacsp.edu.br>
2024-07-30 21:59:50 -0300

    funçcão para o banco de dados

--- delecao.php
+++ delecao.php
@@ -1,21 +1,18 @@
 <?php
-// DELETE FROM itens_compra WHERE id = :id
 $dsn = 'mysql:host=localhost;dbname=lista_compras';
 $user = 'usuario_lista_compras';
 $password = 'Senha@123';
 
-try{
+try {
     $pdo = new PDO($dsn, $user, $password);
 
-    $sql = "INSERT INTO itens_compra (nome_produto, quantidade) 
-            VALUES (:nome_produto, :quantidade)";
+    $sql = "DELETE FROM itens_compra WHERE id = :id";
     
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(['nome_produto' => 'abacaxi', 'quantidade' => 15]);
+    $stmt->execute(['id' => 2]);
 
-    echo "Item inserido com sucesso!";
-    
+    echo "Item removido com sucesso!";
 } catch (PDOException $e) {
-    echo ' Falha na conexão: ' . $e->getMessage();
+    echo 'Falha na conexão: ' . $e->getMessage();
 }
 ?>
