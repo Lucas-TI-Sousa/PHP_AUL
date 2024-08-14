f2137b7886eab084ebcd5eaa01abfa14b9dc1d02
lucas <lucas.ssantos189@senacsp.edu.br>
2024-08-13 21:51:21 -0300

    api

--- comprasAPI/api.php
+++ comprasAPI/api.php
@@ -1,24 +1,23 @@
 <?php
-// $acao = $_GET['acao'];
-
-// if($acao == 'dia') {
-//     echo "Hoje é dia de revisão!";
-// } else if($acao == 'api') {
-//     echo "Minha API funciona!";
-// } else {
-//     echo "Ação não implementada";
-// }
 
 require_once 'DatabaseRepository.php';
-$database = new DatabaseRepository();
 
 $acao = $_GET['acao'];
 
 if($acao == 'listar') {
-    echo $database->getAllItems();
+    echo json_encode(DatabaseRepository::getAllItems());
 } else if($acao == 'adicionar') {
-    echo $database->addItem();
+    $data = json_decode(file_get_contents('php://input'), true);
+    echo DatabaseRepository::addItem($data['nome_produto'], $data['quantidade']);
+} else if($acao == 'atualizar') {
+    $id = $_GET['id'];
+    $data = json_decode(file_get_contents('php://input'), true);
+    echo DatabaseRepository::updateItem($id, $data['nome_produto'], $data['quantidade'], $data['comprado']);
+}  else if($acao == 'deletar') {
+    $id = $_GET['id'];
+    echo DatabaseRepository::deleteItem($id);    
 } else {
     echo "Ação não implementada";
 }
+
 ?>
